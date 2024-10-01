8db4f81b1b386aa900e7d7bf1d3929d39e6b019b
Human <sarim.xs@gmail.com>
2024-09-30 19:57:35 -0600

    Set handler to void return

--- src/types/WSlashCommand.ts
+++ src/types/WSlashCommand.ts
@@ -3,21 +3,17 @@ import {
   CommandInteraction,
   RESTPostAPIApplicationCommandsJSONBody,
 } from 'discord.js';
+import { WDiscordAccessControlList } from './WDiscordAccessControlList';
 
 // A combined type of the actual command payload and some internal properties.
 export type WSlashCommand = RESTPostAPIApplicationCommandsJSONBody & {
   // Handler function to fire on a valid 'interactionCreate'.
   handler: (
     interaction: CommandInteraction,
-  ) => Promise<CommandInteraction | ButtonInteraction>;
+  ) => Promise<void>;
 
   // Access control to impose before the handler is executed. Tells the user "Unauthorized" if check is not passed.
-  access_control?:
-    | string[]
-    | 'VERIFIED_AND_ABOVE'
-    | 'INSIDER_AND_ABOVE'
-    | 'MOD_AND_ABOVE'
-    | 'ADMIN';
+  access_control?: WDiscordAccessControlList;
 
   // Enforce guild-only support before the handler is executed. Tells the user "This command has been disabled for DMs" if check is not passed.
   disabled_in_dm?: boolean;
