a594efd1c7bd002ced34a390672efb3008205b90
Sarim <sarim.xs@gmail.com>
2024-10-09 11:44:45 -0600

    Rename functions

--- src/core/discord-servers.ts
+++ src/core/discord-servers.ts
@@ -13,35 +13,35 @@ export class DiscordServers {
   static Submission: WDiscordServer = {
     name: 'Submission',
     id: '911268076933230662',
-    channels: () => this.getChannelsFn(this.Submission.id),
-    channel: (channelId) => this.getChannelFn(this.Submission.id, channelId),
-    members: () => this.getMembersFn(this.Submission.id),
-    member: (memberId) => this.getMemberFn(this.Submission.id, memberId),
+    channels: () => this.fnGetChannels(this.Submission.id),
+    channel: (channelId) => this.fnGetChannel(this.Submission.id, channelId),
+    members: () => this.fnGetMembers(this.Submission.id),
+    member: (memberId) => this.fnGetMember(this.Submission.id, memberId),
   };
 
   static WikiSubmissionDevelopers: WDiscordServer = {
     name: 'WikiSubmission Developers',
     id: '1080271049377202177',
-    channels: () => this.getChannelsFn(this.WikiSubmissionDevelopers.id),
+    channels: () => this.fnGetChannels(this.WikiSubmissionDevelopers.id),
     channel: (channelId) =>
-      this.getChannelFn(this.WikiSubmissionDevelopers.id, channelId),
-    members: () => this.getMembersFn(this.WikiSubmissionDevelopers.id),
+      this.fnGetChannel(this.WikiSubmissionDevelopers.id, channelId),
+    members: () => this.fnGetMembers(this.WikiSubmissionDevelopers.id),
     member: (memberId) =>
-      this.getMemberFn(this.WikiSubmissionDevelopers.id, memberId),
+      this.fnGetMember(this.WikiSubmissionDevelopers.id, memberId),
   };
 
-  private static async getChannelsFn(
+  private static async fnGetChannels(
     guildId: string,
   ): Promise<GuildChannelManager | null> {
-    const guild = await this.resolveGuild(guildId);
+    const guild = await this.fnResolveGuild(guildId);
     return guild?.channels || null;
   }
 
-  private static async getChannelFn(
+  private static async fnGetChannel(
     guildId: string,
     channelId: string,
   ): Promise<GuildBasedChannel | null> {
-    const guild = await this.resolveGuild(guildId);
+    const guild = await this.fnResolveGuild(guildId);
     return (
       guild?.channels.cache.get(channelId) ||
       (await guild?.channels.fetch(channelId)) ||
@@ -49,18 +49,18 @@ export class DiscordServers {
     );
   }
 
-  private static async getMembersFn(
+  private static async fnGetMembers(
     guildId: string,
   ): Promise<GuildMemberManager | null> {
-    const guild = await this.resolveGuild(guildId);
+    const guild = await this.fnResolveGuild(guildId);
     return guild?.members || null;
   }
 
-  private static async getMemberFn(
+  private static async fnGetMember(
     guildId: string,
     memberId: string,
   ): Promise<GuildMember | null> {
-    const guild = await this.resolveGuild(guildId);
+    const guild = await this.fnResolveGuild(guildId);
     return (
       guild?.members.cache.get(memberId) ||
       (await guild?.members.fetch(memberId)) ||
@@ -68,7 +68,7 @@ export class DiscordServers {
     );
   }
 
-  private static async resolveGuild(guildId: string): Promise<Guild | null> {
+  private static async fnResolveGuild(guildId: string): Promise<Guild | null> {
     const bot = WBotManager.private;
 
     let guild =
